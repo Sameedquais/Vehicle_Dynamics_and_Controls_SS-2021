d199f9bccf9104a541be95b323504b8541bedd44
Sameed Quais <59144973+Sameedquais@users.noreply.github.com>
2021-05-16 22:46:23 +0500

    Exercise Sheet 1

---
+++ Sameed_Ex_Sheet_1/init.m
@@ -0,0 +1,11 @@
+% constants
+length_rare = 0;
+length_front = 2 ;
+input_velocity = 10;
+kp = -0.0235;
+kd = -0.0238;
+
+%initial conditions
+initial_x0 = 0;
+initial_y0 = -0.1; %-0.1 inorder to keep close to the reference point
+initial_psi = 0;
